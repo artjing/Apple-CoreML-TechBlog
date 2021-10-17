1d367cc9dfcc71f78baa57974601317723894e48
dongjingjing <dongjingjing2010@gamil.com>
2021-10-17 12:37:28 -0700

    modify

--- CoreMlConcerterPlayground/CoreMLModelMaker.playground/Contents.swift
+++ CoreMlConcerterPlayground/CoreMLModelMaker.playground/Contents.swift
@@ -1,14 +1,33 @@
 import Cocoa
 import CreateML
 
-let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/tousei/Documents/IOS面试资料/代码练习/TweetSentimentModelMaker/twitter-sanders-apple3.csv"))
+//  use the local path of your /cvs/json file
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "(local path).csv"))
+
+// define the training data and testing data, give it a split
 let(trainingData, testingData) = data.randomSplit(by: 0.9, seed: 5)
+
+// define the text and label base on your colums in the cvs file
 let sentimentClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
+
+// define the metrics
 let evaluationMetrics = sentimentClassifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
+
+// calculate the accuracy
 let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+// degine some basic info of your model
 let metadata = MLModelMetadata(author: "Jing Dong", shortDescription: "A model trained to classify sentiment on Tweets", version: "1.0")
-try sentimentClassifier.write(to: URL(fileURLWithPath: "/Users/tousei/Documents/IOS面试资料/代码练习/TweetSentimentModelMaker/tweetsSentimentClassifier.mlmodel"))
-try sentimentClassifier.prediction(from: "@Apple is terrible company")
-try sentimentClassifier.prediction(from: "@I just found the rest restorant ever, and it's @DuckandWaffle")
+
+// define the ouuput path of model
+try sentimentClassifier.write(to: URL(fileURLWithPath: "(localpath/name).mlmodel"))
+
+// give it a test
+try sentimentClassifier.prediction(from: "@I am so depressed")
+
+//
+try sentimentClassifier.prediction(from: "@So great day")
+
+//
 try sentimentClassifier.prediction(from: "@happy")
 
